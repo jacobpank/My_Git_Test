aba0fcdeec97f9a2636550614c97a8e8073f4a22
Jacob <jacob.pank@gmail.com>
2026-02-08 11:28:40 +0200

    Added items click selection

--- app/src/main/java/com/example/my_git_test/SecondActivity.java
+++ app/src/main/java/com/example/my_git_test/SecondActivity.java
@@ -3,15 +3,21 @@ package com.example.my_git_test;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SecondActivity extends AppCompatActivity {
+    private MenuItem item1;
+    private MenuItem item2;
+    private MenuItem item3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,16 +30,40 @@ public class SecondActivity extends AppCompatActivity {
             return insets;
         });
 
+        getIntentInfo(); // Get intent info from MainActivity
+
+        initMenu(); // Initialize menu items
+    }
+
+    private void getIntentInfo() {
         Intent intent = getIntent();
         intent.getExtras();
         String message = intent.getStringExtra("message");
         int number = intent.getIntExtra("number", 0);
         Toast.makeText(this, "message = " + message + "\nnumber = " + number, Toast.LENGTH_LONG).show();
     }
+    private void initMenu() {
+        item1 = findViewById(R.id.item1);
+        item2 = findViewById(R.id.item2);
+        item3 = findViewById(R.id.item3);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.my_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+            if (id == R.id.item1) {
+                Toast.makeText(this, "Item1 Clicked", Toast.LENGTH_SHORT).show();
+            } else if (id == R.id.item2) {
+                Toast.makeText(this, "Item2 Clicked", Toast.LENGTH_SHORT).show();
+            } else if (id == R.id.item3) {
+                Toast.makeText(this, "Item3 Clicked", Toast.LENGTH_SHORT).show();
+            }
+        return super.onOptionsItemSelected(item);
     }
 }
